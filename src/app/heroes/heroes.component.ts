386d75b58170b5f5d1a87f586b367c8ea7625e8b
Yves Wegner <yves_wegner@sluz.ch>
2024-01-22 16:04:08 +0100

    22.01.2024 Nr. 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero/hero';
 import { HEROES } from '../mock-heroes/mock-heroes';
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   /*standalone: true,*/
@@ -12,10 +14,24 @@ import { FormsModule } from '@angular/forms';
   /*imports: [FormsModule, NgIf, NgFor, UpperCasePipe],*/
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService,
+  ) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
